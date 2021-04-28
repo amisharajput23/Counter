d7dcbd8c472d335c63ce38b234b29821686534e0
amisha <rajputamisha34@gmail.com>
2021-04-28 16:26:21 +0530

    First commit

---
+++ src/store/reducers/counter.js
@@ -0,0 +1,41 @@
+import * as actionTypes from '../actions';
+
+
+const initialState = {
+    counter: 0,
+    
+};
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        
+        case actionTypes.INCREMENT:
+            const newState =Object.assign({}, state);//this will clone the old object in an immutable way giving us new js obj having all the properties of old obj but is technically a diff obj
+            newState.counter = state.counter + 1;
+            return newState;//another way of adding new property
+         
+    case actionTypes.DECREMENT:
+        return {
+            ...state, //distributing all properties and value from the old state to the new formed state and 
+            //adding new property defined below in counter only not in result[]
+            counter: state.counter - 1
+        }
+        case actionTypes.ADD:
+            return {
+                ...state,
+                counter: state.counter + action.val
+            }
+            case actionTypes.SUBTRACT:
+                return {
+                    ...state,
+                    counter: state.counter - action.val
+                }
+
+                
+                 default: return state;
+            }
+};
+
+
+
+export default reducer;
---
+++ src/store/reducers/result.js
@@ -0,0 +1,39 @@
+import * as actionTypes from '../actions';
+
+
+const initialState = {
+    
+    results: []
+};
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        
+     
+
+                case actionTypes.STORE_RESULT:
+                    return{
+                       ...state,
+                       //to update an array immutably using concat
+                       results: state.results.concat({ id: new Date(), value: action.result}) //returns a new array + the arg added to concat; retruns new array with all the properties of old and new state 
+                       //we could have used push() but this manipulates the original value
+                       //
+                    }
+                    case actionTypes.DELETE_RESULT:
+                        //const id = 2;
+                        //const newArray = [...state.results];
+
+                        //newArray.splice(id,1)
+                        const updatedArray = state.results.filter(result => result.id !== action.id);
+                       return{
+                           ...state,
+                           results: updatedArray
+                       } 
+
+                 default: return state;
+            }
+};
+
+
+
+export default reducer;
